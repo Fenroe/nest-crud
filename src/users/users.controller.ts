8f790085ed0bb2eae9862474dea743b650aa7f4f
fenroe <scrambles9494@gmail.com>
2023-04-04 23:50:41 +0300

    individual methods of user module explained

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,11 +6,13 @@ import { User } from './user.entity';
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
+    // get all users
     @Get()
     async findAll(): Promise<User[]> {
         return this.usersService.findAll();
     }
 
+    // get user by id
     @Get(':id')
     async findOne(@Param('id') id: number): Promise<User> {
         const user = await this.usersService.findOne(id);
@@ -21,16 +23,19 @@ export class UsersController {
         }
     }
 
+    // create user
     @Post()
     async create(@Body() user: User): Promise<User> {
         return this.usersService.create(user);
     }
 
+    // update user
     @Put(':id')
     async update(@Param('id') id: number, @Body() user: User): Promise<any> {
         return this.usersService.update(id, user);
     }
 
+    // delete user
     @Delete(':id')
     async delete(@Param('id') id: number): Promise<any> {
         const user = await this.usersService.findOne(id);
