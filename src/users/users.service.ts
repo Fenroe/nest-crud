eb1cd4b1a37f532bdecaa9f5bd7e00d1c2bd98d2
fenroe <scrambles9494@gmail.com>
2023-04-04 22:50:44 +0300

    user service populated and explained

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectRepository(User)
+        private userRepository: Repository<User>
+    ) {}
+
+    async findAll(): Promise<User[]> {
+        return this.userRepository.find();
+    }
+
+    async findOne(id: number): Promise<User> {
+        return this.userRepository.findOne({ where: { id }});
+    }
+
+    async create(user: Partial<User>): Promise<User> {
+        const newuser = this.userRepository.create(user);
+        return this.userRepository.save(newuser);
+    }
+
+    async update(id: number, user: Partial<User>): Promise<User> {
+        await this.userRepository.update(id, user);
+        return this.userRepository.findOne({ where: { id } });
+    }
+
+    async delete(id: number): Promise<void> {
+        await this.userRepository.delete(id);
+    }
+}
+
+/* 
+Injectable() tells NestJS that this is a service
+
+InjectRepository(User) and Repository(User) tells NestJS to inject the repository of the User entity
+
+Created a UserService with five methods: findAll, findOne, create, update and delete
+*/
