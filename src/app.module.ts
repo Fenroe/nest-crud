1d3821715ec53d48b9df63e1b9f4278f2cc460d1
fenroe <scrambles9494@gmail.com>
2023-04-05 00:08:14 +0300

    main app.mdoule file populated and explained

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,36 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as any,
+      host: process.env.PG_HOST,
+      port: parseInt(process.env.PG_PORT),
+      username: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DB,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+/*
+---Explanation---
+Importing the ConfigModule, UsersModule and TypeOrmModule into the file as well as the imports array
+
+Using the forRoot() method to tell NestJS that we want to use the default connection
+
+Define some environment variables that will be set in the docker-compose.yml file to connect to database
+
+Synchronize option set to true so that the database schema automatically updates when application starts
+*/
