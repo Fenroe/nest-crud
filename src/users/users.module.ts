75a37f8e56a9a908ad65fb4e9b1df43a48eb0e7e
fenroe <scrambles9494@gmail.com>
2023-04-04 23:55:31 +0300

    user module populated and explained

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity'
+
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UsersService]
 })
 export class UsersModule {}
+
+/*
+---Explanation---
+Importing the TypeOrmModule and the User entity, UserController and UserServie were already imported
+
+@Module() used to tell NestJS that this is a module
+
+TypeOrmModule.forFeature([User]) added to imports array to tell NestJS to use the User entity
+*/
